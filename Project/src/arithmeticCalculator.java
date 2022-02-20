2956095af0bcc4eb7f38fdb4c7b463ae8d912882
silvee <silveemohapatra@gmail.com>
2022-02-20 15:01:30 +0000

    commit

---
+++ Project/src/arithmeticCalculator.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class arithmeticCalculator {
+
+	public static void main(String[] args) {
+		int num1;
+        int num2;
+        String operation;
+
+
+       Scanner sc = new Scanner(System.in);
+        System.out.println("please enter the first number");
+        num1 = sc.nextInt();
+
+        System.out.println("please enter the second number");
+        num2 = sc.nextInt();
+       
+        
+        System.out.println("Please enter operation");
+        operation = sc.next();
+        
+        if (operation.equals("+"))
+        {
+            System.out.println("your answer is" + (num1 + num2));
+        }
+        else if  (operation == "-")
+        {
+            System.out.println("your answer is" + (num1 - num2));
+        }
+        else if (operation == "*")
+        {
+            System.out.println("your answer is" + (num1 * num2));
+        }
+        else if (operation == "/")
+        {
+            System.out.println("your answer is" + (num1 / num2));
+        }
+        else
+        {
+        	System.out.println("default");
+        }
+
+	}
+
+}
