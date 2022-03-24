4f8c56664b51f0e1aa8b07b9048ac8f6d4154222
silvee <silveemohapatra@gmail.com>
2022-03-24 12:00:28 +0000

    commit

--- FlyAwayBooking/src/com/flightbooking/servlet/ForgotPassword.java
+++ FlyAwayBooking/src/com/flightbooking/servlet/ForgotPassword.java
@@ -1,12 +1,14 @@
 package com.flightbooking.servlet;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.HttpSession;
+import com.flightbooking.database.ConnectionUtil;
 /**
  * Servlet implementation class ForgotPassword
  */
@@ -35,7 +37,24 @@ public class ForgotPassword extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		String email=request.getParameter("email");
+		String password=request.getParameter("password");
+		try {
+		ConnectionUtil dao=new ConnectionUtil();
+		HttpSession session=request.getSession();
+		if(dao.changeAdminPassword(email,password)) {
+		session.setAttribute("message", "Password Changed Successfully");
+		}
+		else {
+		session.setAttribute("message", "Invalid Details");
+		}
+		} catch (ClassNotFoundException | SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		}
+		response.sendRedirect("AdminPage.jsp");
+		}
+
 	}
 
 }
--- FlyAwayBooking/src/com/flightbooking/servlet/LoginOfAdmin.java
+++ FlyAwayBooking/src/com/flightbooking/servlet/LoginOfAdmin.java
@@ -1,11 +1,14 @@
 package com.flightbooking.servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import com.flightbooking.database.ConnectionUtil;
 
 /**
  * Servlet implementation class LoginOfAdmin
@@ -35,7 +38,21 @@ public class LoginOfAdmin extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		String email=request.getParameter("email");
+		String password=request.getParameter("password");
+		try {
+		ConnectionUtil dao=new ConnectionUtil();
+		if(dao.checkAdmin(email,password)) {
+		response.sendRedirect("AdminHome.jsp");
+		}
+		else {
+		HttpSession session=request.getSession();
+		session.setAttribute("message", "Invalid Details");
+		response.sendRedirect("AdminPage.jsp");
+		}
+		} catch (ClassNotFoundException | SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	}
 
 }
